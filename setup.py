159d9aee329e0cb8e79ecedfc4544b1006174e96
Ananda Khosuri <34385321+nandakho@users.noreply.github.com>
2024-09-04 13:55:31 +0700

    fixing setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@
 import setuptools
 
 setuptools.setup(
-	name="extension_xtts_simple",
+	name="extension_parler_tts",
     packages=setuptools.find_namespace_packages(),
 	version="0.0.1",
 	author="rsxdalv",
-	description="XTTS-RVC-UI is a Gradio UI for XTTSv2",
-	url="https://github.com/rsxdalv/extension_xtts_rvc_ui",
+	description="Parler-TTS is a training and inference library for high-fidelity text-to-speech (TTS) models.",
+	url="https://github.com/rsxdalv/extension_parler_tts",
     project_urls={},
     scripts=[],
     install_requires=[
-        "coqui-tts",
+        "parler-tts @ git+https://github.com/huggingface/parler-tts.git",
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
